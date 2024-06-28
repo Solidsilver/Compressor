25953559269e89b8096db9d1f12de9016bae1158
Luke Mattfeld <lukemattfeld@gmail.com>
2024-06-28 16:15:22 -0700

    added basic command for giving compressed blocks

--- src/main/java/com/solidsilver/papermc/CompressListener.kt
+++ src/main/java/com/solidsilver/papermc/CompressListener.kt
@@ -9,6 +9,7 @@ import org.bukkit.persistence.PersistentDataType
 
 class CompressListener(private val plugin: Compressor): Listener {
 
+
     @EventHandler
     fun onPlace(event: BlockPlaceEvent) {
         if (event.itemInHand.itemMeta.persistentDataContainer.has(plugin.getCompLevelKey())) {
@@ -19,29 +20,30 @@ class CompressListener(private val plugin: Compressor): Listener {
 
     @EventHandler
     fun asCraft(event: PrepareItemCraftEvent) {
-        val items = event.inventory.matrix
-        var numCompItems = 0
-        var cmpLvl = 0
-        var itemType: Material? = null
-        for (item in items) {
-            if (item?.itemMeta?.persistentDataContainer?.has(plugin.getCompLevelKey()) == true) {
-                val itmLvl = item.itemMeta.persistentDataContainer.get(plugin.getCompLevelKey(), PersistentDataType.INTEGER) ?: 0
-                if (itemType == null) {
-                    itemType = item.type
-                    cmpLvl = itmLvl
-                } else if (cmpLvl != itmLvl || itemType != item.type) {
+            var numCompItems = 0
+            var cmpLvl = 0
+            var itemType: Material? = null
+            for (item in event.inventory.matrix) {
+                if (item?.itemMeta?.persistentDataContainer?.has(plugin.getCompLevelKey()) == true) {
+                    val itmLvl = item.itemMeta.persistentDataContainer.get(
+                        plugin.getCompLevelKey(),
+                        PersistentDataType.INTEGER
+                    ) ?: 0
+                    if (itemType == null) {
+                        itemType = item.type
+                        cmpLvl = itmLvl
+                    } else if (cmpLvl != itmLvl || itemType != item.type) {
+                        break
+                    }
+                    numCompItems += 1
+                } else {
                     break
                 }
-                numCompItems += 1
-            } else {
-                break
             }
-        }
-        if (numCompItems == 9) {
-            event.inventory.result = itemType?.let { plugin.createCompressedItem(it, cmpLvl + 1) }
-        } else if (numCompItems > 1) {
-            event.inventory.result = null
-        }
-
+            if (numCompItems == 9 && cmpLvl < Compressor.MAX_LEVEL) {
+                event.inventory.result = itemType?.let { plugin.createCompressedItem(it, cmpLvl + 1) }
+            } else if (numCompItems in 2..8 ) {
+                event.inventory.result = null
+            }
     }
 }
